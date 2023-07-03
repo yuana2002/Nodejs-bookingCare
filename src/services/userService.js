d9c021dde99b8dc543690d1f1d2c49c8cf2e2471
yuana2002 <trinhhuuquoc36@gmail.com>
2023-07-03 14:50:46 +0700

    60

--- src/services/userService.js
+++ src/services/userService.js
@@ -160,7 +160,7 @@ let deleteNewUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'missing required parameter'
@@ -174,6 +174,10 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
                 await user.save();
                 // await db.User.save({
                 //     firstName: data.firstName,
