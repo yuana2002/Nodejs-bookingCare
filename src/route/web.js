708b3ba4e566dce7f55ac14743059030f2e0e64f
yuana2002 <trinhhuuquoc36@gmail.com>
2023-07-14 12:37:03 +0700

    95

--- src/route/web.js
+++ src/route/web.js
@@ -37,6 +37,8 @@ let initWebRoutes = (app) => {
 
     router.post('/api/create-new-specialty', specialtyController.createSpecialty);
     router.get('/api/get-specialty', specialtyController.getAllSpecialty);
+    router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
+
 
 
 
